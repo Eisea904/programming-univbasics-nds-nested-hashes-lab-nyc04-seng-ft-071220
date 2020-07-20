46843e288e1e0eec2b6d2ccefc70d73ea53b36b7
Alex Eisenhauer <eisea904@newschool.edu>
2020-07-20 13:28:32 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -26,12 +26,13 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-  binding.pry
+  #binding.pry
   aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  binding.pry 
   aoh[row][key] = new_value
 end
